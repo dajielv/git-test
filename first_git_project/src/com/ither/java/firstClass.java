683eb99bc9295cd2827d4af53efadce95e630f30
uc <uc@itcast.com>
2018-11-04 22:11:03 +0800

    在firstClass类中新增方法test

--- first_git_project/src/com/ither/java/firstClass.java
+++ first_git_project/src/com/ither/java/firstClass.java
@@ -8,4 +8,9 @@ public class firstClass {
         System.out.println("hello word!");
         System.out.println("hello word!");
     }
+
+    public void test(){
+        int i = 0;
+        System.out.println(i);
+    }
 }

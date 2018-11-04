4c3935882080cd1c47b3ad56324c7ba36cbbf021
uc <uc@itcast.com>
2018-11-04 22:08:50 +0800

    本地工程添加到git

---
+++ first_git_project/src/com/ither/java/firstClass.java
@@ -0,0 +1,11 @@
+package com.ither.java;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+    }
+}

74ff591bd3fc993b63a69cc889918c55ceffe77f
uc <uc@itcast.com>
2018-11-05 08:57:24 +0800

    在主分支master上做的修改

--- first_git_project/src/com/ither/java/firstClass.java
+++ first_git_project/src/com/ither/java/firstClass.java
@@ -2,20 +2,6 @@ package com.ither.java;
 
 public class firstClass {
     public static void main(String[] args) {
-        System.out.println("hello word!");
-        System.out.println("hello word!");
-        System.out.println("hello word!");
-        System.out.println("hello word!");
-        System.out.println("hello word!");
-        test();
-        //添加注解
-
+        System.out.println("这是在主分支master上做的修改2018.11.5 8:56");
     }
-
-
-    public static void test(){
-        int i = 0;
-        System.out.println(i);
-    }
-
 }
